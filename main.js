cfb72c5c6fce35e7a0e06a85baeea5b41a2bf890
Pakku73 <12monstroplantes@gmail.com>
2023-11-22 21:22:48 +0100

    blabla2

---
+++ main.js
@@ -0,0 +1,8 @@
+// let pseudo = "Bob"
+// alert(pseudo)
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const usernameInput = document.getElementById('username');
+//     const passwordInput = document.getElementById('password');
+
+// });
