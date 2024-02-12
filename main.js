9d9457359c6964fe46c0f01fdd85c7d9726629b8
Pakku73 <12monstroplantes@gmail.com>
2024-02-12 20:33:33 +0100

    Final_commit

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-// let pseudo = "Bob"
-// alert(pseudo)
+const usernameInput = document.getElementById("username")
+const passwordInput = document.getElementById("password")
+const btn = document.getElementById("showPassword")
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const usernameInput = document.getElementById('username');
-//     const passwordInput = document.getElementById('password');
+passwordInput.addEventListener("input", (event) =>{
+    if (event.target.value) {
+        btn.classList.remove("hidden")
+    } else{
+        btn.classList.add("hidden")
+    }
+});
 
-// });
+btn.addEventListener("click", (event) =>{
+    document.querySelector('#showPassword').textContent = "Afficher"
+    if (passwordInput.getAttribute("type") === "password"){
+        passwordInput.setAttribute("type", "text")
+        document.querySelector('#showPassword').textContent = "Masquer"
+    } else{
+        passwordInput.setAttribute("type", "password")
+    }
+});
